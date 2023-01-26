7beeeab4fb73af816ea2d25950baff5305699f2c
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2023-01-26 03:08:39 +0100

    Added floating navbar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Nav from './components/Nav/Nav';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <Nav />
       <Skills />
       <Projects />
       <Contact />
