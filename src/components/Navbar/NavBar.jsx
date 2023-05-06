db5942597f420c1bc5e0cb4eccb0d9c63464d429
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2023-05-06 12:59:14 +0200

    Removed scrollspy
    Gets a lot of bugs

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -4,16 +4,11 @@ import logo from '../../assets/img/logo_2_transparent.png'
 import navIcon1 from '../../assets/img/nav-icon1.svg'
 import CV from '../../assets/doc/Curriculum Paolo Compagnoni.pdf'
 import './navbar.css'
-import ScrollspyNav from "react-scrollspy-nav";
 
 const NavBar = () => {
     
-    // useState imposta una variabile di stato activeLink e una funzione setActiveLink che lo aggiorna
-
     const [activeLink, setActiveLink] = useState('');
     const [scrolled, seScrolled] = useState(false);
-
-    //con useEffect diciamo a React che il componente deve fare qualcosa dopo il suo render
 
     useEffect(() => {
         const onScroll = () => {
@@ -36,52 +31,57 @@ const NavBar = () => {
     }
 
     return (
+        
         <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
-        <Container>
-            <Navbar.Brand href="#">
-                <img src={logo} alt="logo" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" >
-                <span className="navbar-toggler-icon"></span>
-            </Navbar.Toggle>
-            <ScrollspyNav
-                scrollTargetIds={["home", "about", "experience", "services", "portfolio", "testimonials"]}
-                offset={0}
-                activeNavClass="active"
-                scrollDuration="100"
-                headerBackground="false"
-            >
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#home" className={"navbar-link"} 
-                                onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : "navbar-link"} 
-                                onClick={() => onUpdateActiveLink('about')}>Chi sono</Nav.Link>
-                <Nav.Link href="#experience" className={activeLink === 'experience' ? 'active navbar-link' : "navbar-link"} 
-                                onClick={() => onUpdateActiveLink('experience')}>Competenze</Nav.Link>
-                <Nav.Link href="#services" className={activeLink === 'services' ? 'active navbar-link' : "navbar-link"} 
-                                onClick={() => onUpdateActiveLink('services')}>Servizi</Nav.Link>
-                <NavDropdown title="Altro">
-                    <NavDropdown.Item href="#portfolio" className={"navbar-link"} 
-                                onClick={() => onUpdateActiveLink('portfolio')}>Portfolio</NavDropdown.Item>
-                    <NavDropdown.Item href="#testimonials" className={"navbar-link"}
-                                onClick={() => onUpdateActiveLink('testimonials')}>Feedback</NavDropdown.Item>                    
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item download={'Curriculum Paolo Compagnoni'} href={CV}>Scarica CV</NavDropdown.Item>
-            </NavDropdown>
-            </Nav>         
-            </Navbar.Collapse>
-            </ScrollspyNav>
-            <span className='navbar-text'>
-                <div className='social-icon'>
-                    <a href='https://www.linkedin.com/in/paolo-compagnoni-3749631b2/' target='_blank' rel="noreferrer"><img src={navIcon1} alt="linkedIn" /></a>
-                </div>
-                <div className="navBtns">
-                    <a href="#contact"><button ><span>Contattami</span></button></a>
-                </div>
-            </span>
-        </Container>
-        </Navbar>        
+            <Container>
+                <Navbar.Brand href="#">
+                    <img src={logo} alt="logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" >
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+                
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home" className={"navbar-link"} 
+                                        onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                        <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : "navbar-link"} 
+                                        onClick={() => onUpdateActiveLink('about')}>Chi sono</Nav.Link>
+                        <Nav.Link href="#experience" className={activeLink === 'experience' ? 'active navbar-link' : "navbar-link"} 
+                                        onClick={() => onUpdateActiveLink('experience')}>Competenze</Nav.Link>
+                        <Nav.Link href="#services" className={activeLink === 'services' ? 'active navbar-link' : "navbar-link"} 
+                                        onClick={() => onUpdateActiveLink('services')}>Servizi</Nav.Link>
+                        <NavDropdown title="Altro">
+                            <NavDropdown.Item href="#portfolio" className={"navbar-link"} 
+                                        onClick={() => onUpdateActiveLink('portfolio')}>Portfolio</NavDropdown.Item>
+                            <NavDropdown.Item href="#testimonials" className={"navbar-link"}
+                                        onClick={() => onUpdateActiveLink('testimonials')}>Feedback</NavDropdown.Item>                    
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item download={'Curriculum Paolo Compagnoni'} href={CV}>Scarica CV</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>         
+                    
+            
+                        <span className='navbar-text'>
+                            <div className='social-icon'>
+                                <a href='https://www.linkedin.com/in/paolo-compagnoni-3749631b2/' target='_blank' rel="noreferrer">
+                                    <img src={navIcon1} alt="linkedIn" />
+                                </a>
+                            </div>
+                            <div className="navBtns">
+                                <a href="#contact">
+                                    <button>
+                                        <span>Contattami</span>
+                                    </button>
+                                </a>
+                            </div>
+                        </span>
+            
+                </Navbar.Collapse>
+                  
+            </Container>
+        </Navbar>
+             
     );
 }
 
